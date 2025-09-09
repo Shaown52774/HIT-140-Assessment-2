f9c4d8ddbc2b98084126658b49ac70cc52e7ab3b
Al-Amin Dhaly <shaownimtiaz726@gmail.com>
2025-09-10 00:53:46 +1000

    Imtiaz Part Done

---
+++ Assignment_2.py
@@ -0,0 +1,20 @@
+# Imtiaz- Import Libraries
+from google.colab import files
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import statsmodels.api as sm
+from scipy import stats
+# Imtiaz- Upload Datasets
+uploaded = files.upload()  # upload dataset1.csv + dataset2.csv
+d1 = pd.read_csv("dataset1.csv")
+d2 = pd.read_csv("dataset2.csv")
+# Imtiaz- Clean columns
+d1.columns = d1.columns.str.lower().str.strip()
+d2.columns = d2.columns.str.lower().str.strip()
+# Imtiaz- Dataset overview
+print("Dataset1 shape:", d1.shape)
+print("Dataset2 shape:", d2.shape)
+print(d1.head(), "\n")
+print(d2.head())
